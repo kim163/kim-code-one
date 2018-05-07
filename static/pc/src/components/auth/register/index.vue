e4ece75c8f7af80b4f803a7726c148502b254bc4
clover <clover@exmple.com>
2018-05-07 18:09:02 +0800

    注册 语言更新         注注册 逻辑更新

--- static/pc/src/components/auth/register/index.vue
+++ static/pc/src/components/auth/register/index.vue
@@ -5,28 +5,28 @@
         <i class="iconfont icon-close"></i>
       </div>
       <div class="pop-content">
-        <h2 class="alert-title">{{$t('register.title')}} <span class="fr smalltxt">{{$t('register.hasAccount')}}？ <a class="a-login">{{$t('register.loginNow')}}</a></span></h2>
+        <h2 class="alert-title">{{$t('register.title')}} <span class="fr smalltxt">{{$t('register.hasAccount')}}？ <a class="a-login" @click="showLoginDialog=true">{{$t('register.loginNow')}}</a></span></h2>
         <div class="tab-box">
           <span v-for="item in registerType" @click="registerItem=item.value" class="s" :class="{active:registerItem==item.value}" :key="item.value">
             {{generateTitle(item.name)}}
           </span>
         </div>
         <div class="form-box form-box-phone">
-          <div class="form-group" v-show="registerItem=='phone'">
+          <div class="form-group" v-if="registerItem=='phone'">
             <p class="form-subtitle">{{$t('register.inputPhoneNum')}}</p>
             <div class="form-input">
               <label class="select-country" id="select-country" country_id="+86">
                 中国 +86
                 <a href="javascript:void(0) " class="arrow"></a>
               </label>
-              <input type="text" class="ps-input cl-blue fl" v-model="data.account" :placeholder="$t('register.inputPlaceholder1')" maxlength="20" />
+              <input type="text" class="ps-input cl-blue fl" v-model="data.phone" :placeholder="$t('register.inputPlaceholder1')" maxlength="20" />
             </div>
 
           </div>
-          <div class="form-group" v-show="registerItem=='email'">
+          <div class="form-group" v-if="registerItem=='email'">
             <p class="form-subtitle">{{$t('register.inputEmail')}}</p>
             <div class="form-input">
-              <input type="text" class="ps-input" v-model="data.account" :placeholder="$t('register.inputPlaceholder0')" maxlength="20" />
+              <input type="text" class="ps-input" v-model="data.email" :placeholder="$t('register.inputPlaceholder0')" maxlength="20" />
             </div>
 
           </div>
@@ -52,7 +52,7 @@
             <p class="form-subtitle">{{$t('register.AgainPassword')}}</p>
             <div class="form-input">
             <input ref="pwd" type="password" autocomplete="off" class="ps-input fl inpt-pwd"
-                   v-model="data.password" :placeholder="$t('register.inputPlaceholder4')" maxlength="16" />
+                   v-model="data.confirmPassword" :placeholder="$t('register.inputPlaceholder4')" maxlength="16" />
             <!--<eyes :dom="$refs.pwd"></eyes>-->
             </div>
           </div>
@@ -65,10 +65,10 @@
           <!--</div>-->
           <div class="agreement">
             <input type="checkbox" checked>
-            我已阅读并同意 <a class="a-login">服务条款</a>
+            {{$t('register.readAgreenment')}} <a class="a-login">{{$t('register.agreenment')}}</a>
           </div>
           <span class="validate"></span>
-          <input type="submit" class="submit btn btn-block" @click.prevent="register" id="submit_user" value="立即注册">
+          <input type="submit" class="submit btn btn-block" @click.prevent="register" id="submit_user" :value="$t('register.register')">
         </div>
       </div>
     </div>
@@ -89,7 +89,7 @@
           {name: "register.phoneReg", value: "phone"},
           {name: "register.emailReg", value: "email"}
         ],
-        registerItem: 'phone',
+        registerItem: 'email',
         data: {
           phone: "", //true string
           email: "", //true string
@@ -102,10 +102,20 @@
     methods: {
       generateTitle,
       checked() {
-        if (!check.account.test(this.data.account)) toast("登入账号的长度请介于6-10字符之间！");
-        else if (!check.password.test(this.data.password)) toast("密码需为6-16位，包含数字和字母");
-        //else if (!check.phone.test(this.data.phone)) toast("手机号格式不正确");
-        else if (this.data.imageCode == "") toast("请输入验证码");
+          if(this.registerItem =='email'){
+            if (!this.data.email || this.data.email=='')
+              toast("邮箱地址不允许为空");
+          }else{
+            if (!this.data.phone || this.data.phone=='')
+              toast("手机号不允许为空");
+          }
+
+        if (!this.data.imageCode || this.data.imageCode=='')
+          toast("验证码不能为空");
+        else if (!this.data.password || this.data.password=='')
+          toast("请输入密码");
+        else if (!this.data.confirmPassword || this.data.confirmPassword=='')
+          toast("请输入确认密码");
         else {
           return true;
         }
@@ -113,7 +123,7 @@
       register() {
         if (!this.checked()) return;
         register(this.data).then((res) => {
-          this.getimg()
+          //this.getimg()
           if (res.success) {
             this.$emit('input', false)
             this.$store.dispatch("UPDATE_USERDATA");
@@ -122,15 +132,12 @@
             toast(res.message);
           }
         }).catch(err => {
-          this.getimg()
+          //this.getimg()
         });
       },
       getCaptcha(e) {
         // 发送验证码  判断是否输入手机号
         //this.authImg = getAuthImg();
-      },
-      toggleTab: function(tab) {
-        this.currentTab = tab; // tab 为当前触发标签页的组件名
       }
     },
     components: {
