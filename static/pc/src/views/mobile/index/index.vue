2589b36133ad4f4cb82d01fbe1a193c918cc7dc8
anne <anne@9an.com>
2018-05-23 18:09:48 +0800

    新增 mobile 公共样式

--- static/pc/src/views/mobile/index/index.vue
+++ static/pc/src/views/mobile/index/index.vue
@@ -1,23 +1,42 @@
 <template>
   <div class="mtrading-hall">
-      <m-headnav>{{$t('navbar.tradingHall')}}</m-headnav>
+     <m-headnav>{{$t('navbar.tradingHall')}}</m-headnav>
+     <div class="mobilenav-tabs">
+        <span v-for="(item,i) in transactType" @click="transactItem=item.value" :class="{active:transactItem==item.value}" :key="item.value">
+            {{generateTitle(item.name)}}
+        </span>
+     </div>
+
       交易大厅
+
+     <m-navbar></m-navbar>
   </div>
 </template>
 <script>
   import mHeadnav from 'components/m-headnav';
+  import mNavbar from 'components/m-navbar';
+
+  import { generateTitle } from '@/util/i18n';
 
   export default {
     data() {
-      return {};
+      return {
+        transactType:[
+          {name:'transactionHome.buyUet', value: 'buyUet' },
+          {name:'transactionHome.saleUet', value: 'saleUet' }
+        ],
+        transactItem: 'buyUet',
+      };
     },
     props: {},
-    methods: {},
+    methods: {
+      generateTitle,
+    },
     computed: {},
     created() {
     },
     components: {
-      mHeadnav
+      mHeadnav, mNavbar
     }
   };
 </script>
@@ -25,5 +44,4 @@
   .mtrading-hall{
 
   }
-
 </style>
