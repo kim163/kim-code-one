3b080f3a555c6a15c86d9a011c74f0f9fa7f446e
Polo <1040294@NN.COM>
2018-05-25 15:35:20 +0800

    修改我的挂单

--- static/pc/src/views/mobile/my-pending-orders/index.vue
+++ static/pc/src/views/mobile/my-pending-orders/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="my-pending-orders">
-
+    <header>{{$t('transactionRecord.tranPending')}}</header>
   </div>
 </template>
 
 <script>
+  import Header from 'components/m-header'
   export default {
     name: "index",
     data() {
 
     },
+    components:{
+      Header
+    }
   }
 </script>
 
--- static/pc/src/views/mobile/transaction-record/index.vue
+++ static/pc/src/views/mobile/transaction-record/index.vue
@@ -63,7 +63,7 @@
     computed: {
       ...mapGetters([
         "userData"
-      ])
+      ]),
     },
     data() {
       return {
