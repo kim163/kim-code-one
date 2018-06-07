301888f93be0c541d1ff1b07bbd2afab247f6d68
Polo <1040294@NN.COM>
2018-06-07 20:48:53 +0800

    绑定成功之后  进入久安交易大厅

--- static/pc/src/views/mobile/business-bind/bind-success.vue
+++ static/pc/src/views/mobile/business-bind/bind-success.vue
@@ -6,6 +6,7 @@
     </div>
     <div class="btn-list">
       <router-link class="bind-def-btn" :to="{name:'mBindList'}">查看绑定记录</router-link>
+      <router-link class="bind-def-btn" :to="{name:'mIndex'}">进入交易大厅</router-link>
       <a class="go-business" :href="callBackUrl">返回商户</a>
     </div>
   </div>
