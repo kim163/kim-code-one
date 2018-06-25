1e95f8fcabf0f7e6c64ac33f60e2e9641a0e37fc
anne <anne@9an.com>
2018-06-25 17:46:22 +0800

    H5 注册页面手机号码默认为 86

--- static/pc/src/views/mobile/register/index.vue
+++ static/pc/src/views/mobile/register/index.vue
@@ -96,7 +96,7 @@
           imageCode: "", //true string
           password: "", //true string
           confirmPassword: "",// true string
-          areaCode:"+63"
+          areaCode:"+86"
         },
         requestdata: {}
       }
