b7bc5d716db7db28eedc902f9cc7792df4698697
Polo <1040294@NN.COM>
2018-06-26 18:09:36 +0800

    优化登录

--- static/pc/src/views/mobile/login/login-inline.vue
+++ static/pc/src/views/mobile/login/login-inline.vue
@@ -143,12 +143,10 @@
 
               $localStorage.set('tokenInfo', JSON.stringify(res.data.tokenVo));
               $localStorage.set('userData', JSON.stringify(aesutil.encrypt(res.data.userId)))
-              this.$store.dispatch('UPDATE_USERDATA');
+              this.$store.dispatch('CHECK_ONLINE', true);
+              this.$store.dispatch('UPDATE_TOKEN_INFO', res.data.tokenVo);
+              this.$store.commit('SET_USERDATA',res.data);
 
-              // this.$router.replace({path:"/mh/"});
-              //this.$router.push({name: 'mIndex'});
-
-              //  window.location.href = "/mh/";
             } else {
               toast(res.message)
             }
