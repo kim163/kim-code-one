73e8ffee7f252192e9784df1917a977501aecfc2
Polo <1040294@NN.COM>
2018-06-29 18:03:05 +0800

    测试vue bus

--- static/pc/src/components/balance/index.vue
+++ static/pc/src/components/balance/index.vue
@@ -10,8 +10,8 @@
   export default {
     data() {
       return {
-        balance:'0',
-        assetCode:''
+        balance: '0',
+        assetCode: ''
       }
     },
     computed: {
@@ -23,50 +23,69 @@
       }
     },
     methods: {
-      getBalance(order){
-          let userId = this.userId;
-          const userData = this.userData
-          this.assetCode = userData.accountChainVos[0].assetCode
-          this.request = {
-            userId: userId,
-            assetCode: this.assetCode,
-            address: userData.accountChainVos[0].address,
-          }
-          transaction.getRealBalance(this.request).then(res => {
-            console.log(res)
-            if(res.code == '10000'){
-              this.$nextTick(() => {
-                this.balance=res.data.key;
-                // this.assetCode=res.data[0].assetCode;
-                this.$emit('getBalance',this.balance)
-              })
-            }else{
-              this.balance='刷新后再试';
-            }
-
-          }).catch(error => {
-            toast(error.message);
-          });
+      getBalance(order) {
+        let userId = this.userId;
+        const userData = this.userData
+        this.assetCode = userData.accountChainVos[0].assetCode
+        this.request = {
+          userId: userId,
+          assetCode: this.assetCode,
+          address: userData.accountChainVos[0].address,
         }
+        transaction.getRealBalance(this.request).then(res => {
+          console.log(res)
+          if (res.code == '10000') {
+            this.$nextTick(() => {
+              this.balance = res.data.key;
+              // this.assetCode=res.data[0].assetCode;
+              this.$emit('getBalance', this.balance)
+            })
+          } else {
+            this.balance = '刷新后再试';
+          }
 
+        }).catch(error => {
+          toast(error.message);
+        });
+      },
+      onBus(){
+        Vue.$global.bus.$on('update:balance', () => {
+          this.getBalance()
+        })
+      },
+      offBus(){
+        Vue.$global.bus.$off('update:balance', () => {
+          this.getBalance()
+        })
+      }
     },
     created() {
       if (this.islogin) {
         this.getBalance()
       }
-      Vue.$global.bus.$on('update:balance', () => {
-        this.getBalance()
-      })
+      debugger
+      this.onBus()
     },
     mounted() {
 
     },
-    components: {
+    components: {},
+    // activated(){
+    //   debugger
+    //   this.onBus()
+    // },
+    // deactivated(){
+    //   debugger
+    //   this.offBus()
+    // },
+    beforeDestroy() {
+      debugger
+      this.offBus()
     }
   };
 </script>
 
-<style scoped lang="scss" >
+<style scoped lang="scss">
 
 </style>
 
