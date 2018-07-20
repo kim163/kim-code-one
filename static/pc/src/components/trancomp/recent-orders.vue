674da0e827fa985f2ac7cb339fe092b8bd5ddcb7
Polo <1040294@NN.COM>
2018-07-20 15:55:56 +0800

    修改交易大厅最近订单

--- static/pc/src/components/trancomp/recent-orders.vue
+++ static/pc/src/components/trancomp/recent-orders.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="recent-orders">
-      <h3 class="title"> {{$t('transactionHome.recentOrders')}} </h3>
-      <ul>
-         <router-link tag="li" :to="{name:'orderDetail',params:{id:order.id}}" v-for="(order,i) in OrderList.data||[]">
-           <div class="fl">
-             <p>
-               <span class="btn btn-border" v-show="order.credit == userData.userId">买入</span>
-               <span class="btn btn-orange" v-show="order.debit == userData.userId">卖出</span>
-             </p>
-             <p>
-               {{order.creditAccountNameTwin}}
-             </p>
-             <p>
-               等待付款
-             </p>
-           </div>
-           <div class="fr">
-              <p>09:45</p>
-              <p><span class="amount"> {{order.creditAmount}} </span>  UET</p>
-           </div>
-         </router-link>
-      </ul>
+    <h3 class="title"> {{$t('transactionHome.recentOrders')}} </h3>
+    <ul v-if="OrderList.data.length > 0">
+      <router-link tag="li" :to="{name:'orderDetail',params:{id: order.id}}" v-for="(order,i) in OrderList.data||[]">
+        <div class="fl">
+          <p>
+            <span class="btn btn-border" v-show="order.credit == userData.userId">{{$t('transactionRecord.buy')}}</span>
+            <span class="btn btn-orange" v-show="order.debit == userData.userId">{{$t('transactionRecord.sale')}}</span>
+          </p>
+          <p>
+            {{order.creditName}}
+          </p>
+          <div>
+            <div class="status" v-if="order.status != 61">
+              {{(order.status === 45 ? $t('transactionRecord.waitingForPayment') :
+              $t('transactionRecord.waitingForRelease'))}}
+            </div>
+            <div class="time" v-else>申诉锁定</div>
+          </div>
+        </div>
+        <div class="fr">
+          <p>
+            <countdown v-if="order.status != 61" :end-time="order.intervalTime-order.elapsedTime"
+                       end-text="过期"></countdown>
+          </p>
+          <p><span class="amount"> {{order.creditAmount}} </span> UET</p>
+        </div>
+      </router-link>
+    </ul>
+    <no-data-tip tip="暂无订单" v-else></no-data-tip>
   </div>
 </template>
 <script>
-  import { transaction } from 'api';
-  import {mapGetters,mapActions,mapMutations} from 'vuex';
+  import {transaction} from 'api';
+  import {mapGetters, mapActions, mapMutations} from 'vuex';
+  import Countdown from 'components/countdown'
+  import NoDataTip from 'components/no-data-tip'
   export default {
     data() {
       return {
@@ -37,16 +47,16 @@
     },
     props: {},
     methods: {
-      getOrderList(index){
-        let userId=this.userData.userId;
-        this.request={
+      getOrderList(index) {
+        let userId = this.userData.userId;
+        this.request = {
           limit: 10,
-          offset:0,
-          credit:userId,
-          debit:userId,
-          types:[11,12]
+          offset: 0,
+          credit: userId,
+          debit: userId,
+          types: [11, 12]
         }
-        if(!isNaN(index)) {
+        if (!isNaN(index)) {
           this.request.offset = (index - 1) * this.request.limit;
         }
         transaction.getOrderxPage(this.request).then(res => {
@@ -60,48 +70,51 @@
 
     },
     computed: {
-      ...mapGetters(["userData","islogin"])
+      ...mapGetters(["userData", "islogin"])
     },
     created() {
       this.getOrderList();
     },
-    components: {}
+    components: {
+      Countdown,
+      NoDataTip
+    }
   };
 </script>
 <style lang="scss">
-.recent-orders{
-  .title{
-    height: 50px;
-    line-height: 50px;
-    font-size: 16px;
-    color: #ffffff;
-    background: #5087FF;
-    padding: 0 19px;
-  }
-  ul{
-    li{
-      height: 110px;
-      padding: 20px 19px;
-      border-bottom: 1px solid #D4D4D4;
-      cursor: pointer;
-      &:last-child{
-        border: none;
-      }
-      &:hover{
-        background: #F8F8F8;
-      }
-      .btn-border{
-        color: #5087ff;
-      }
-      .btn-orange{
-        color: #ff2a00;
-      }
-      .amount{
-        font-size: 24px;
-        color: #000000;
+  .recent-orders {
+    .title {
+      height: 50px;
+      line-height: 50px;
+      font-size: 16px;
+      color: #ffffff;
+      background: #5087FF;
+      padding: 0 19px;
+    }
+    ul {
+      li {
+        height: 110px;
+        padding: 20px 19px;
+        border-bottom: 1px solid #D4D4D4;
+        cursor: pointer;
+        &:last-child {
+          border: none;
+        }
+        &:hover {
+          background: #F8F8F8;
+        }
+        .btn-border {
+          color: #5087ff;
+        }
+        .btn-orange {
+          color: #ff2a00;
+        }
+        .amount {
+          font-size: 24px;
+          color: #000000;
+        }
       }
     }
   }
-}
 
 </style>
