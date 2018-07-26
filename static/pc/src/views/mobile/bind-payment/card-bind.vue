29e012836a1c2e08fee7e8d3ec34c407596f0e58
Polo <1040294@NN.COM>
2018-07-26 11:55:57 +0800

    修改绑定银行卡校验逻辑

--- static/pc/src/views/mobile/bind-payment/card-bind.vue
+++ static/pc/src/views/mobile/bind-payment/card-bind.vue
@@ -11,7 +11,7 @@
           </div>
           <div class="card-item">
             <label class="title">银行名称:</label>
-            <input class="card-input" type="text" v-model.trim="bank" @mouseover="getBankName"/>
+            <input class="card-input" type="text" v-model.trim="bank" @focus="getBankName(false)"/>
           </div>
           <div class="card-item">
             <label class="title">持卡人姓名:</label>
@@ -105,6 +105,14 @@
           toast('请点击上传您的收款二维码')
           return false
         }
+
+        if(this.type === 3){
+          this.getBankName(true)
+        }else{
+          this.toBind()
+        }
+      },
+      toBind(){
         const data = {
           userId: this.userData.userId,
           account: this.account,
@@ -139,24 +147,34 @@
           toast(err)
         })
       },
-      getBankName(){
-        const data = {
-          bankNo:this.account
-        }
-        autoRecognize(data).then(res => {
-          console.log(res)
-          if(res.code === 10000){
-            if(res.data && !_.isEmpty(res.data)){
-              this.bank = res.data.bankName
-            }else{
-              toast('您输入的银行卡号不能识别，请重新输入')
-            }
-          }else{
-            toast(res.message)
+      getBankName(isToBind){
+        if(this.account != ''){
+          const data = {
+            bankNo:this.account
           }
-        }).catch(err => {
-          toast(err)
-        })
+          autoRecognize(data).then(res => {
+            console.log(res)
+            if(res.code === 10000){
+              if(res.data && !_.isEmpty(res.data)){
+                this.bank = res.data.bankName
+                if(isToBind){
+                  this.toBind()
+                }
+              }else{
+                this.bank = ''
+                toast('您输入的银行卡号不能识别，请重新输入')
+              }
+            }else{
+              toast(res.message)
+              this.bank = ''
+            }
+          }).catch(err => {
+            toast(err)
+            return false
+          })
+        }else{
+          toast('请输入银行卡号')
+        }
       }
     },
     created(){
