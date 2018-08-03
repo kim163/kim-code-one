0250e26f4c44a503ba1e407f0815c5316f99d838
Polo <1040294@NN.COM>
2018-08-03 16:04:29 +0800

    活动添加图片及页面布局

--- static/pc/src/views/mobile/activity-center/index.vue
+++ static/pc/src/views/mobile/activity-center/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <m-headnav></m-headnav>
+    <m-headnav>优惠活动</m-headnav>
     <div class="header">
       <div class="activity-balance">
         <div class="title">赠币活动剩余</div>
@@ -8,9 +8,9 @@
           <span class="balance">239104</span> UET
         </div>
       </div>
-      <div class="marquee">
+      <div class="adv-marquee">
         <i class="iconfont"></i>
-        <div class="msg-list"></div>
+        <div class="msg-list">用户“我是周星驰”在快速交易中获得优惠券1000...</div>
       </div>
     </div>
     <div class="activity-list">
@@ -42,5 +42,36 @@
   @import "~assets/scss/mobile";
   .header{
     width: 100%;
+    .activity-balance{
+      width: 100%;
+      color: $white;
+      background: #3573FA;
+      @include f(18px);
+      text-align: center;
+      padding:r(30) 0;
+      .title{
+        margin-bottom: r(15);
+      }
+      .balance{
+        @include f(30px);
+      }
+    }
+    .adv-marquee{
+      width: 100%;
+      height: r(40);
+      padding: 0 r(10);
+      display: flex;
+      align-items: center;
+      .iconfont{
+        width: r(14);
+        height: r(16);
+        background: url("~images/system-info.png") no-repeat;
+        background-size: cover;
+      }
+      .msg-list{
+        width: calc(100% - #{r(14)});
+        height: 100%;
+      }
+    }
   }
 </style>
