c1e4ade6dc998f8a1e9f24706d9e79d9d67fe169
Polo <1040294@NN.COM>
2018-08-07 15:42:49 +0800

    修改活动页面布局

--- static/pc/src/views/mobile/activity-center/index.vue
+++ static/pc/src/views/mobile/activity-center/index.vue
@@ -13,14 +13,18 @@
         <div class="msg-list">用户“我是周星驰”在快速交易中获得优惠券1000...</div>
       </div>
     </div>
-    <div class="activity-list">
-
+    <div class="activity-list p-def">
+      <div class="activity-item" v-for="item in 4" :key="item">
+        <div class="title" :class="{buy: item === 1}"></div>
+      </div>
     </div>
+    <m-navbar></m-navbar>
   </div>
 </template>
 
 <script>
   import mHeadnav from 'components/m-headnav';
+  import mNavbar from 'components/m-navbar';
   export default {
     name: "activity-center",
     data(){
@@ -29,7 +33,8 @@
       }
     },
     components:{
-      mHeadnav
+      mHeadnav,
+      mNavbar
     },
     methods:{
 
@@ -74,6 +79,28 @@
         @include f(14px);
         line-height: r(40);
         padding-left: r(10);
+      }
+    }
+  }
+  .activity-list{
+    .activity-item{
+      width: 100%;
+      margin-bottom: r(10);
+      .title{
+        min-height: r(90);
+        border-radius: r(6);
+        &.buy{
+          background-image: linear-gradient(44deg, #FEE140 0%, #FCA96D 50%, #FCA96D 50%, #FA709A 100%);
+        }
+        &.sell{
+          background-image: linear-gradient(45deg, #69EDFF 0%, #AD7EFD 100%);
+        }
+        &.withdrawal{
+          background-image: linear-gradient(47deg, #EE69FF 0%, #955AF9 100%);
+        }
+        &.recharge{
+          background-image: linear-gradient(-236deg, #FB9F75 0%, #FA709A 100%);
+        }
       }
     }
   }
