8966e425bc89104aeacf400d4c9a39568770228a
Polo <1040294@NN.COM>
2018-08-14 10:47:36 +0800

    修改快速买卖 卖家要求没有默认选择bug

--- static/pc/src/views/mobile/pending-orders/index.vue
+++ static/pc/src/views/mobile/pending-orders/index.vue
@@ -312,7 +312,7 @@
       if(!_.isUndefined(withdraw) && withdraw === 'true'){
         if(this.pendingItem === 'seller'){
           this.minSellAmount = 1
-          this.proofType = 1
+          this.proofType = '1'
         }
       }
     },
