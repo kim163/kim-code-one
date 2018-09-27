8c1db26d12a9591d06df38d9818962685ebc8e18
anne <anne@9an.com>
2018-09-27 11:24:31 +0800

    隐私登录请求接口参数调整

--- static/pc/src/api/show.js
+++ static/pc/src/api/show.js
@@ -14,7 +14,8 @@ export const login = jsonData => service({
 export const implicitLogin = jsonData => service({
   url: numversion + `user/chain/implicitLogin`,
   method: 'post',
-  data: jsonData
+  data: jsonData,
+  encryptDef:true  //使用默认加密规则
 });  // 隐式登录
 
 export const loginByUserNameAndPwd = jsonData => service({
