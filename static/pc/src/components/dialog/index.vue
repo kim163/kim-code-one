4503a708fc68677c5a51da713665d0a28bbce1cb
Polo <1040294@NN.COM>
2018-09-28 16:16:04 +0800

    开发定制版pc端用户登录

---
+++ static/pc/src/components/dialog/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <popup>
+    <div class="dialog-main">
+      <a class="close iconfont icon-close" @click="$emit('change',false)"></a>
+      <slot></slot>
+    </div>
+  </popup>
+</template>
+
+<script>
+  import Popup from 'components/common-popup'
+  export default {
+    name: "index",
+    components:{
+      Popup
+    },
+    props:{
+      show:{
+        type:Boolean,
+        default:false
+      }
+    },
+    model:{
+      props:"show",
+      event:'change'
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .dialog-main{
+    padding: 30px;
+    background: #FFFFFF;
+    position: relative;
+    width: 506px;
+    .close{
+      font-size: 20px;
+      position: absolute;
+      top: 30px;
+      right: 30px;
+      cursor: pointer;
+      z-index: 2;
+      color: #C8C8C8;;
+    }
+  }
+</style>
