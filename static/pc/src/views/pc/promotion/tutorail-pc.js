ce4e7fd8de9c4dbb128d5b0b24ca74e4ed436736
Polo <1040294@NN.COM>
2018-10-25 13:40:56 +0800

    添加配置图片

--- static/pc/src/views/pc/promotion/tutorail-pc.js
+++ static/pc/src/views/pc/promotion/tutorail-pc.js
@@ -1,4 +1,5 @@
-import pc11 from '@/assets/images/tutorail/pc/pic-1-1.png'
+import pc11 from '@/assets/images/tutorail/pc/pc-1-1.png'
+import pc12 from '@/assets/images/tutorail/pc/pc-1-2.png'
 const pc1 = [
   {
     title:'1.打开合作商户网站，点击“存款”，执行存款',
@@ -6,7 +7,7 @@ const pc1 = [
   },
   {
     title:'2.打开合作商户网站，点击“存款”，执行存款',
-    img: pc11
+    img: pc12
   }
 ]
 
