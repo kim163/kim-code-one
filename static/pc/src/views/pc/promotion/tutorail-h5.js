b44431f804c3823ce0b25a14981080e6a3da93f8
Polo <1040294@NN.COM>
2018-10-29 15:06:24 +0800

    修改H5教程配置参数

--- static/pc/src/views/pc/promotion/tutorail-h5.js
+++ static/pc/src/views/pc/promotion/tutorail-h5.js
@@ -1,26 +1,26 @@
-import H511 from '@/assets/images/tutorail/app/app-1-1.png'
-import H512 from '@/assets/images/tutorail/app/app-1-2.png'
-import H513 from '@/assets/images/tutorail/app/app-1-3.png'
-import H514 from '@/assets/images/tutorail/app/app-1-4.png'
-const h51 = [
+import H5_11 from '@/assets/images/tutorail/app/app-1-1.png'
+import H5_12 from '@/assets/images/tutorail/app/app-1-2.png'
+import H5_13 from '@/assets/images/tutorail/app/app-1-3.png'
+import H5_14 from '@/assets/images/tutorail/app/app-1-4.png'
+const h5_1 = [
   {
     title:'1.打开合作商户网站，点击“存款”，执行存款',
-    img: H511
+    img: H5_11
   },
   {
     title:'2.打开合作商户网站，点击“存款”，执行存款',
-    img: H512
+    img: H5_12
   },
   {
     title:'3.打开合作商户网站，点击“存款”，执行存款',
-    img: H513
+    img: H5_13
   },
   {
     title:'4.打开合作商户网站，点击“存款”，执行存款',
-    img: H514
+    img: H5_14
   },
 ]
 
 export default {
-  h51
+  h5_1
 }
